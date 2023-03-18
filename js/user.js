1474846f91aebf80cf2699c2f9d3a2e93e43d5aa
Bartos Martin <bmartin042503@gmail.com>
2023-03-18 12:36:09 +0100

    külön js fájlban égetett user tömb

---
+++ js/user.js
@@ -0,0 +1,23 @@
+class User {
+    constructor(nem, nev, szulDatum, email, ismerosokSzama, profilkepUrl) {
+        this.nem = nem;
+        this.nev = nev;
+        this.szulDatum = szulDatum;
+        this.email = email;
+        this.ismerosokSzama = ismerosokSzama;
+        this.profilkepUrl = profilkepUrl || "../img/profile/default.png"
+    }
+}
+
+const users = [
+    new User(0, "Nagy Béla", "1999. 05. 25.", "bela@example.com", 456, "../img/profile/default"),
+    new User(1, "Kovács Anna", "1998. 02. 14.", "anna@example.com", 102, "../img/profile/default"),
+    new User(0, "Szabó Péter", "2001. 11. 08.", "peter@example.com", 34, "../img/profile/default"),
+    new User(2, "Tóth Lili", "2003. 09. 19.", "lili@example.com", 789, "../img/profile/default"),
+    new User(0, "Kovács Gábor", "1995. 12. 01.", "gabor@example.com", 65, "../img/profile/default"),
+    new User(0, "Kertész András", "2002. 03. 27.", "andras@example.com", 432, "../img/profile/default"),
+    new User(1, "Varga Boglárka", "2000. 08. 10.", "boglarka@example.com", 987, "../img/profile/default"),
+    new User(1, "Balázs Judit", "1994. 06. 17.", "judit@example.com", 23, "../img/profile/default"),
+    new User(0, "Molnár Zsolt", "1997. 01. 09.", "zsolt@example.com", 156, "../img/profile/default"),
+    new User(0, "Kovács József", "2000. 10. 02.", "istvan@example.com", 347, "../img/profile/default")
+];
